498325fb95b6849923801b70213d430f4a55076c
TheRealFamakan <camarafamakan2@gmail.com>
2025-04-02 00:16:45 +0200

    cible.php

--- cible.php
+++ cible.php
@@ -1,9 +1,20 @@
 <?php
-    $Nom= htmlspecialchars($_POST['Nom']);
-    $Prenom= htmlspecialchars($_POST["Prenom"]);
-    $Email= htmlspecialchars($_POST["Email"]);
-    echo"<h2>Les informations saisis :</h2>  <br>";
-    echo"Nom : " . $Nom . "<br>";
-    echo"Prenom : " . $Prenom . "<br>";
-    echo"Email : " . $Email . "<br>";
-?>
+     $Nom= htmlspecialchars($_POST['Nom']);
+     $Prenom= htmlspecialchars($_POST["Prenom"]);
+     $Email= htmlspecialchars($_POST["Email"]);
+     echo"<h2>Les informations saisis :</h2>  <br>"; 
+     if(empty($Nom)){
+        echo"Il manque votre nom<br>";
+     }
+     if(empty($Prenom)){
+        echo"Il manque votre Prenom<br>";
+     }
+     if(empty($Email)){
+        echo"Il manque votre Email<br>";
+     }   
+     if(!empty($Nom) && !empty($Prenom) && !empty($Email)){
+        echo"Nom : " . $Nom . "<br>";
+        echo"Prenom : " . $Prenom . "<br>";
+        echo"Email : " . $Email . "<br>";
+     }    
+ ?>
