7b2c19d31358e6f8a49bd552826865260d5b738e
TheRealFamakan <camarafamakan2@gmail.com>
2025-04-05 22:37:35 +0200

    Update cible.php

--- cible.php
+++ cible.php
@@ -4,34 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <style>
-        body{
-            background-color: antiquewhite;
-            text-align: left;
-            font-family: Arial;
-        }
-    </style>
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $Nom = isset($_POST["Nom"]) ? htmlspecialchars($_POST["Nom"]) : "";
+        $Prenom = isset($_POST["Prenom"]) ? htmlspecialchars($_POST["Prenom"]) : "";
+        $Email = isset($_POST["Email"]) ? htmlspecialchars($_POST["Email"]) : "";
+    } else {
+        header("Location: index.php");
+        exit();
+    }
+?>
+    <form action="cible.php" method="post"><br>
+        <h1>Les inforamation saisis</h1>
+        <div class="input-box">
+        <input type="text" name="Nom" value="<?=$Nom?>" ><br>
+        </div>
+        <div class="input-box">
+        <input type="text"  name="Prenom" value="<?=$Prenom?>" ><br>
+        </div>
+        <div class="input-box">
+        <input type="text"  name="Email"  value="<?=$Email?>" ><br>
+        </div>
+        <div class="input-box">
+        <input class="sirr-btn" type="submit" value="Update">
+        </div>
+    </form>
 </body>
 </html>
-<?php
-    $Nom= htmlspecialchars($_POST['Nom']);
-    $Prenom= htmlspecialchars($_POST["Prenom"]);
-    $Email= htmlspecialchars($_POST["Email"]);
-    echo"<h1>Les informations saisis :</h1>  <br>"; 
-    if(empty($Nom)){
-        echo"Il manque votre nom<br>";
-    }
-    if(empty($Prenom)){
-        echo"Il manque votre Prenom<br>";
-    }
-    if(empty($Email)){
-        echo"Il manque votre Email<br>";
-    }   
-    if(!empty($Nom) && !empty($Prenom) && !empty($Email)){
-        echo"<br> Nom : " . $Nom . "<br>";
-        echo"Prenom : " . $Prenom . "<br>";
-        echo"Email : " . $Email . "<br>";
-    }    
-?>
+
