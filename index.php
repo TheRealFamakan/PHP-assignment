6eff030bf55941745f0ad4f934744ff6fae2f5a5
TheRealFamakan <camarafamakan2@gmail.com>
2025-04-05 18:10:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulaire</title>
+    <title>Formulaire </title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form action="cible.php" method="post"><br>
-        Nom :<input type="text" name="Nom" ><br>
-        Prenom: <input type="text" name="Prenom" ><br>
-        Email : <input type="text" name="Email" ><br>
-        <input type="submit" value="Sirr">
+        <h1>Formulaire</h1>
+        <div class="input-box">
+        <input type="text" name="Nom" placeholder="Nom" ><br>
+        </div>
+        <div class="input-box">
+        <input type="text"  name="Prenom" placeholder="Prenom" ><br>
+        </div>
+        <div class="input-box">
+        <input type="text"  name="Email"  placeholder="Email" ><br>
+        </div>
+        <div class="input-box">
+        <input class="sirr-btn" type="submit" value="Sirr">
+        </div>
     </form>
-
 </body>
 </html>
 
