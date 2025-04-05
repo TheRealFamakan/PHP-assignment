f35d7a8a53cf2dd3eebf79c184b8f3c318921ffc
TheRealFamakan <camarafamakan2@gmail.com>
2025-04-05 18:09:34 +0200

    Update cible.php

--- cible.php
+++ cible.php
@@ -1,20 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        body{
+            background-color: antiquewhite;
+            text-align: left;
+            font-family: Arial;
+        }
+    </style>
+</body>
+</html>
 <?php
-     $Nom= htmlspecialchars($_POST['Nom']);
-     $Prenom= htmlspecialchars($_POST["Prenom"]);
-     $Email= htmlspecialchars($_POST["Email"]);
-     echo"<h2>Les informations saisis :</h2>  <br>"; 
-     if(empty($Nom)){
+    $Nom= htmlspecialchars($_POST['Nom']);
+    $Prenom= htmlspecialchars($_POST["Prenom"]);
+    $Email= htmlspecialchars($_POST["Email"]);
+    echo"<h1>Les informations saisis :</h1>  <br>"; 
+    if(empty($Nom)){
         echo"Il manque votre nom<br>";
-     }
-     if(empty($Prenom)){
+    }
+    if(empty($Prenom)){
         echo"Il manque votre Prenom<br>";
-     }
-     if(empty($Email)){
+    }
+    if(empty($Email)){
         echo"Il manque votre Email<br>";
-     }   
-     if(!empty($Nom) && !empty($Prenom) && !empty($Email)){
-        echo"Nom : " . $Nom . "<br>";
+    }   
+    if(!empty($Nom) && !empty($Prenom) && !empty($Email)){
+        echo"<br> Nom : " . $Nom . "<br>";
         echo"Prenom : " . $Prenom . "<br>";
         echo"Email : " . $Email . "<br>";
-     }    
- ?>
+    }    
+?>
